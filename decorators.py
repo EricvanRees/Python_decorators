c969a4a9a6ce7efb2ebb2382fa321ef84918dfad
Eric van Rees <ericvanrees@gmail.com>
2025-06-11 09:27:49 +0200

    third commit

--- decorators.py
+++ decorators.py
@@ -44,7 +44,7 @@ What is a decorator?  A decorator is a function that takes another function as a
 
 # Example 1 of decorator function
 
-def decorator_function(original_function):
+""" def decorator_function(original_function):
   def wrapper_function():
     return original_function()
   return wrapper_function
@@ -53,4 +53,92 @@ def display():
   print('display function ran')
   
 decorated_display = decorator_function(display)
-decorated_display() # prints "display function ran"
+decorated_display() """ # prints "display function ran"
+
+"""
+Why use a decorator? They allow to easily add functionality to existing functions by adding that functionality inside a wrapper. 
+"""
+
+""" Example 1:
+def decorator_function(original_function):
+  def wrapper_function():
+    print(f'wrapper executed this before {original_function.__name__} ')
+    return original_function()
+  return wrapper_function
+
+# @decorator_function == decorator_function(display)
+def display():
+  print('display function ran')
+  
+decorated_display = decorator_function(display)
+decorated_display()
+
+prints "wrapper executed this before display"
+prints "display function ran" 
+"""
+
+# Example 1, now with @:
+""" def decorator_function(original_function):
+  def wrapper_function():
+    print(f'wrapper executed this before {original_function.__name__} ')
+    return original_function()
+  return wrapper_function
+
+@decorator_function
+def display():
+  print('display function ran')
+
+display() """
+
+"""
+Example 2 with *args and **kwargs
+
+def decorator_function(original_function):
+  # args and kwargs required inside wrapper function definition if the original function is called with any args and kwargs:
+  def wrapper_function(*args, **kwargs):
+    print(f'wrapper executed this before {original_function.__name__} ')
+    # args and kwargs required here too:
+    return original_function(*args, **kwargs)
+  return wrapper_function
+
+@decorator_function
+def display():
+  print('display function ran') 
+  
+@decorator_function
+def display_info(name, age):
+  print(f'display_info ran with arguments {name}, {age}')
+
+
+#info printed in console: 
+#wrapper executed this before display_info 
+#display_info ran with arguments John, 25
+#wrapper executed this before display
+#display function ran
+
+  
+display_info('John', 25)
+display()
+"""
+
+# Example 3: using classes as decorators
+
+class decorator_class(object):
+  
+  def __init__(self, original_function):
+    self.original_function = original_function
+    
+  def __call__(self, *args, **kwds):
+    print(f'call method executed this before {self.original_function.__name__}')
+    return self.original_function(*args, **kwds)
+
+@decorator_class
+def display():
+  print('display function ran')
+  
+@decorator_class
+def display_info(name, age):
+  print(f'display_info ran with arguments {name}, {age}')
+  
+display_info('John', 25)
+display()
